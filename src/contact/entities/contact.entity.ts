57b1c69fd342d7ee912453cc0cbef6e01e6bcf6b
Antoinebac435 <antoinebacquet62@gmail.com>
2024-09-25 15:25:36 +0200

    Update contact.entity.ts

--- src/contact/entities/contact.entity.ts
+++ src/contact/entities/contact.entity.ts
@@ -1,1 +1,18 @@
-export class Contact {}
+import { FormationCenter, Intermediaire, Intervention } from '@prisma/client';
+
+export class Contact {
+  id: string;
+  name: string;
+  photo: string;
+  email: string;
+  phone: string;
+  role: string;
+  linkedin: string;
+  comment: string;
+  createdAt: Date;
+  formationCenter?: FormationCenter;
+  formationCenterId?: string;
+  intermediaire?: Intermediaire;
+  intermediaireId?: string;
+  interventions: Intervention[];
+}
