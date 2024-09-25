b5eeccf1c7d31dc9c91e6ee8245510993acd86fe
Clément Berdin <clement.berdin@gmail.com>
2024-09-25 15:26:42 +0200

    feat(Pricing): add Pricing Entity

--- src/pricing/entities/pricing.entity.ts
+++ src/pricing/entities/pricing.entity.ts
@@ -1,1 +1,13 @@
-export class Pricing {}
+import { FormationCenter, Intermediaire, PricingType } from '@prisma/client';
+
+export class Pricing {
+  id: string;
+  name: string;
+  price: number;
+  type: PricingType;
+  createdAt: Date;
+  formationCenter: FormationCenter;
+  formationCenterId: string;
+  intermediaire: Intermediaire;
+  intermediaireId: string;
+}
