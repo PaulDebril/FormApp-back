4c0ac58f602134accba92dd7680a8822cabd0b72
Antoinebac435 <antoinebacquet62@gmail.com>
2024-09-25 15:27:44 +0200

    Update contract.entity.ts

--- src/contract/entities/contract.entity.ts
+++ src/contract/entities/contract.entity.ts
@@ -1,1 +1,9 @@
-export class Contract {}
+import { Mission } from '@prisma/client';
+
+export class Contract {
+  id: string;
+  name: string;
+  pdf: string;
+  createdAt: Date;
+  missions: Mission[];
+}
