35fb137ddcf6f1382127a677d278b49d6eb6d925
Clément Berdin <clement.berdin@gmail.com>
2024-09-25 15:23:56 +0200

    feat(Task): add task Entity

--- src/task/entities/task.entity.ts
+++ src/task/entities/task.entity.ts
@@ -1,1 +1,11 @@
-export class Task {}
+import { Mission, TaskPriority } from '@prisma/client';
+
+export class Task {
+  id: string;
+  name: string;
+  priority: TaskPriority;
+  deadline: Date;
+  createdAt: Date;
+  mission: Mission;
+  missionId: string;
+}
