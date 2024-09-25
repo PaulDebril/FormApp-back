5280955bf72bc7bfa4d980d423458f96dec21a51
Antoinebac435 <antoinebacquet62@gmail.com>
2024-09-25 15:31:48 +0200

    Update formation.entity.ts

--- src/formation/entities/formation.entity.ts
+++ src/formation/entities/formation.entity.ts
@@ -1,1 +1,13 @@
-export class Formation {}
+import { FormationCenter, Subject, FormationLevel } from '@prisma/client';
+
+export class Formation {
+  id: string;
+  name: string;
+  url: string;
+  level: FormationLevel;
+  documents: string[];
+  createdAt: Date;
+  formationCenter?: FormationCenter;
+  formationCenterId?: string;
+  subjects: Subject[];
+}
